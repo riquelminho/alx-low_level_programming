6110afe6a62cb8b800a0b1ccdcb852d652f729ab
Rachid.EL <riquelminho@users.noreply.github.com>
2023-03-16 13:43:59 +0100

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
 * main - A function that prints a line using pringtf function
-*Return : 0 (Success)
+* Return : 0 (Success)
 **/
-int main(void) 
-{ 
+int main(void)
+{
 printf("with proper grammar, but the outcome is a piece of art,\");
 return (0); }
