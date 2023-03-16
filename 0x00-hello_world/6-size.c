daf34ff2c0b26ab02e86ca6c18da2dea2aa6557e
rachid <rachidriquelmi@gmail.com>
2023-03-16 05:36:34 -0700

    new update

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+/**
+* main - A function that prints the size of various computer types
+* * Return: 0 (Success)
+int main(void)
+{
+char c;
+int i;
+long int li; 
+long long int ll; 
+float f;
+printf("The size of char is %lu byte(c)\n", sizeof(unsigned long)); 
+printf("The size of short is %lu byte(i)\n", sizeof(unsigned long)); 
+printf("The size of int is %lu byte(s)\n", sizeof(unsigned long);
+printf("The size of long is %lu byte(s)\n", sizeof(unsigned long); 
+printf("The size of long long is %lu byte(s)\n", sizeof(unsigned long);
+printf("The size of float is %lu byte(s)\n", sizeof(unsigned long)); 
+return (0);
+}
+
