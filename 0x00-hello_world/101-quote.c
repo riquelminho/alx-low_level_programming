5d43a2a912f7159bc5165866257430ce4eeab4aa
Rachid.EL <riquelminho@users.noreply.github.com>
2023-03-17 00:15:31 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - function that print a line to the standard error
- * Return:1
- */
+* main - function that print a line to the standard error
+* Return:1
+*/
 int main(void)
 {
 fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
