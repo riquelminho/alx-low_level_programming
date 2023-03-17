c2ba3d09524e2fd61f36e42f500957d2e4094a15
rachid <rachidriquelmi@gmail.com>
2023-03-17 16:03:00 -0700

    new update

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - Prints all possible combinations of three different digits,
- *        in ascending order, separated by a comma followed by a space.
- *
- * Return: Always 0.
- */
+* main - Prints all possible combinations of three different digits,
+*        in ascending order, separated by a comma followed by a space.
+*
+* Return: Always 0.
+*/
 int main(void)
 {
 	int digit1, digit2, digit3;
@@ -22,7 +22,6 @@ int main(void)
 
 				if (digit1 == 7 && digit2 == 8 && digit3 == 9)
 					continue;
-				
 				putchar(',');
 				putchar(' ');
 			}
