107447a61af9fa2ff40402f9ea50e1647c1f3da5
Rachid.EL <riquelminho@users.noreply.github.com>
2023-03-17 23:46:26 +0100

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -16,7 +16,6 @@ int main(void)
 		{
 			putchar((digit1 % 10) + '0');
 			putchar((digit2 % 10) + '0');
-
 			if (digit1 == 8 && digit2 == 9)
 				continue;
 			putchar(',');
@@ -24,6 +23,5 @@ int main(void)
 		}
 	}
 	putchar('\n');
-
 	return (0);
 }
