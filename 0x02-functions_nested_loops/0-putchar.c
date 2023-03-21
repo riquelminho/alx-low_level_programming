e6ca0840b3df9e84b4ee237fa2ab87e3ed6529fd
Rachid.EL <riquelminho@users.noreply.github.com>
2023-03-21 11:33:14 +0000

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,21 +4,19 @@
 /**
 
 * main - Entry point
-
-*
-
 * Return: Always 0
 
 */
-int main() {
-    putchar('_');
-    putchar('p');
-    putchar('u');
-    putchar('t');
-    putchar('c');
-    putchar('h');
-    putchar('a');
-    putchar('r');
-    putchar('\n');
+int main()
+{
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
     return 0;
 }
