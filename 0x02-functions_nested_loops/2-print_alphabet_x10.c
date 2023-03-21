64b14a7f87a8263e73840219a69b6672455ba90e
Rachid.EL <riquelminho@users.noreply.github.com>
2023-03-21 22:04:43 +0000

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -17,25 +17,7 @@ for (c = 'a'; c <= 'z'; c++)
 {
 _putchar(c);
 }
-=======
- * print_alphabet_x10 - Print alphabet x10 times
- *
- * Return: always 0
- */
-void print_alphabet_x10(void)
-{
-    char c;
-    int i = 0;
-    while (i <= 9) 
-    {
-        for (c = 'a'; c <= 'z'; c++)
-        {
-            _putchar(c);
-        }
->>>>>>> af042bb562db3879036c406270fbaf1db10fcd11
-
 _putchar('\n');
-
 i++;
 }
 }
