4372979d98e855547c3160726c0f0810c15844c3
Rachid.EL <riquelminho@users.noreply.github.com>
2023-03-24 01:40:08 +0000

    Update 1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-#include "main.h"
-#include <stdio.h>
-
-/**
-* main - check the code
-*
-* Return: Always 0.
-*/
-int _isdigit(int c)
-{
-if (c >= 48 && c <= 57)
-{
-return (1);
-}
-return (0);
-}
-=======
 #include "main.h"
 #include <stdio.h>
 
@@ -34,4 +16,3 @@ int _isdigit(int c)
 	}
 	return (0);
 }
->>>>>>> 07743037b2a630c268409f73521f3186168dc14e
