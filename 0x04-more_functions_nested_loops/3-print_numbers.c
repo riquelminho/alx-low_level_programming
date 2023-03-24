b86535c930cc2244cc7e594538115d01babf47a2
Rachid.EL <riquelminho@users.noreply.github.com>
2023-03-24 01:57:11 +0000

    Update 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers -  checks the code.
+ *
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 48; a < 58; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
