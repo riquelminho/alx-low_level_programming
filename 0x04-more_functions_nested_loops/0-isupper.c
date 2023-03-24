bd72957b93e2ecd360f29840a54239bd25ff75ca
rachid <rachidriquelmi@gmail.com>
2023-03-23 17:43:14 -0700

    first upload

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,7 @@
+int _isupper(int c) {
+    if (c >= 'A' && c <= 'Z') {
+        return 1;   // c is an uppercase character
+    } else {
+        return 0;   // c is not an uppercase character
+    }
+}
