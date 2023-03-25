c31469fb25f45c12fdc9eb87513da5332a77af21
Rachid.EL <riquelminho@users.noreply.github.com>
2023-03-25 16:33:20 +0000

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,27 +7,18 @@
   */
 void jack_bauer(void)
 {
-	int a, b, c, d;
-
-	for (a = 0; a <= 2; a++)
-	{
-		for (b = 0; b <= 9; b++)
-		{
-			if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
-			{
-				for (c = 0; c <= 5; c++)
-				{
-					for (d = 0; d <= 9; d++)
-					{
-						_putchar(a + '0');
-						_putchar(b + '0');
-						_putchar(58);
-						_putchar(c + '0');
-						_putchar(d + '0');
-						_putchar('\n');
-					}
-				}
-			}
-		}
-	}
+    int i, j;
+    for(i=0; i<24; i++)
+    {
+        for(j=0; j<60; j++)
+        {
+            putchar(i / 10 + '0');
+            putchar(i % 10 + '0');
+            putchar(':');
+            putchar(j / 10 + '0');
+            putchar(j % 10 + '0');
+            putchar('\n');
+        }
+    }
+    return 0;
 }
