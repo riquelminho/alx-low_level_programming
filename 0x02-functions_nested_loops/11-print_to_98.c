ab138e5bfa79491fc104438c425fcf9062e91401
Rachid.EL <riquelminho@users.noreply.github.com>
2023-03-26 16:32:02 +0000

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,44 +1,19 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
-  * print_to_98 - Prints all natural numbers from n to 98
-  * @n: The number to start printing from
-  *
-  * Return: Always 0.
-  */
-void print_to_98(int n)
+ * print_times_table - check the code
+ * @n: The multiplication table requested.
+ * Return: Nothing.
+ */
+void print_times_table(int n)
 {
-	if (n <= 98)
-	{
-		for (; n <= 98; n++)
-		{
-			if (n == 98)
-			{
-				printf("%d", n);
-				printf("\n");
-				break;
-			}
-			else
-			{
-				printf("%d, ", n);
-			}
-		}
-	}
-	else
-	{
-		for (; n >= 98; n--)
-		{
-			if (n == 98)
-			{
-				printf("%d", n);
-				printf("\n");
-				break;
-			}
-			else
-			{
-				printf("%d, ", n);
-			}
-		}
-	}
+ if (n <= 98) {
+        for (int i = n; i < 98; i++) {
+            printf("%d, ", i);
+        }
+    } else {
+        for (int i = n; i > 98; i--) {
+            printf("%d, ", i);
+        }
+    }
+    printf("98\n");
 }
