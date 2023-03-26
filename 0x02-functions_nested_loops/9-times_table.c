0acb812e18a56012f3ef3af01279caa57165f05a
Rachid.EL <riquelminho@users.noreply.github.com>
2023-03-26 14:32:28 +0000

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,38 +6,18 @@
   * Return: empty output
   */
 void times_table(void)
-{
-	int x, y, z, u, d;
+{  int i, j, product;
 
-	for (x = 0; x <= 9; x++)
-	{
-		for (y = 0; y <= 9; y++)
-		{
-			z = x * y;
+    for (i = 0; i < 10; i++) {
+        for (j = 0; j < 10; j++) {
+            product = i * j;
+            
+            putchar(product + '0');
 
-			if (z > 9)
-			{
-				u = z % 10;
-				d = (z - u) / 10;
-
-				_putchar(44);
-				_putchar(32);
-				_putchar(d + '0');
-				_putchar(u + '0');
-			}
-			else
-			{
-				if (y != 0)
-				{
-					_putchar(44);
-					_putchar(32);
-					_putchar(32);
-				}
-
-				_putchar(z + '0');
-			}
-		}
-
-		_putchar('\n');
-	}
+            if (j < 9) {
+                printf(", ");
+            }
+        }
+        printf("$\n");
+    }
 }
