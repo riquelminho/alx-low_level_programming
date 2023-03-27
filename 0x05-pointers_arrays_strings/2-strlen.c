3bc1c4869c77b9118f30dbb163b2f72b27cd4a5a
Rachid.EL <riquelminho@users.noreply.github.com>
2023-03-27 15:25:28 +0000

    Update 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen - check the code
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+ int i;
+    for(i = 0; str[i] != '\0'; i++);
+    return i;
+}
