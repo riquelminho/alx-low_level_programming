9b96b8be3f928edacf6dc8be7c4c039106df975d
Rachid.EL <riquelminho@users.noreply.github.com>
2023-03-29 23:35:45 +0000

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,22 +1,22 @@
 #include "main.h"
 /**
-* _strncat - concatenate strings, defining the size of second string.
-* @dest: string with concatenation
-* @src: string to be concatenated
-* @n: size of second string
-* Return: Always 0.
-*/
-char *_strncat(char *dest, char *src, int n)
+ * reverse_array - prints reverse array.
+ * @a: array to be compared.
+ * @n: size of array.
+ * Return: reversed array.
+ */
+void reverse_array(int *a, int n)
 {
-while (*dest != '\0')
-dest++;
-while (n > 0 && *src != '\0')
-{
-*dest = *src;
-dest++;
-src++;
-n--;
-}
-*dest = '\0';
-return (dest);
+	int swap, begin, end;
+
+	begin = 0;
+	end = n - 1;
+	while (begin < end)
+	{
+		swap = *(a + begin);
+		*(a + begin) = *(a + end);
+		*(a + end) = swap;
+		begin++;
+		end--;
+	}
 }
