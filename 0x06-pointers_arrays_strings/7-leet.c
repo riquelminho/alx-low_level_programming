8ac2a158e6406c8b119b925a339e9a3a28d19d6c
Rachid.EL <riquelminho@users.noreply.github.com>
2023-03-29 23:15:59 +0000

    Update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -11,19 +11,19 @@ int i = 0;
 int j = 0;
 char leet_char[5] = {'4', '3', '0', '7', '1'};
 char vowels[5] = {'a', 'e', 'o', 't', 'l'};
-while (str[i] != '\0')
+while (s[i] != '\0')
 {
 j = 0;
 while (j < 5)
 {
-if (str[i] == vowels[j] || str[i] == (vowels[j] - 32))
+if (s[i] == vowels[j] || s[i] == (vowels[j] - 32))
 {
-str[i] = leet_char[j];
+s[i] = leet_char[j];
 break;
 }
 j++;
 }
 i++;
 }
-return (str);
+return (s);
 }
