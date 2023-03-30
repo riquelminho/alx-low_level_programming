183ea96fdfecb47fa9454eefc176217b8809c23b
Rachid.EL <riquelminho@users.noreply.github.com>
2023-03-30 16:11:10 +0000

    Update 104-print_buffer.c

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,65 @@
+#include "main.h"
+/**
+ * print_buffer - add 2 strings.
+ * @b: string1.
+ * @size: string2.
+ * Return: String with all letters in ROT13 base.
+ */
+void print_buffer(char *b, int size)
+{
+  int i, j;
+  for (i = 0; i < size; i += 10)
+  {
+
+    printf("%08x: ", i);
+
+
+    for (j = 0; j < 10; j++)
+    {
+      if (i + j < size)
+      {
+        printf("%02x ", (unsigned char)b[i + j]);
+      }
+      else
+      {
+        printf("   ");
+      }
+
+      if (j == 4)
+      {
+        printf(" "); 
+      }
+    }
+
+
+    printf(" ");
+    for (j = 0; j < 10; j++)
+    {
+      if (i + j < size)
+      {
+        char c = b[i + j];
+        if (c >= ' ' && c <= '~')
+        {
+          printf("%c", c);
+        }
+        else
+        {
+          printf(".");
+        }
+      }
+      else
+      {
+        printf(" ");
+      }
+    }
+
+    // Print the newline character at the end of the line
+    printf("\n");
+  }
+
+  // Print a newline character if the buffer is empty or incomplete
+  if (size <= 0 || i % 10 != 0)
+  {
+    printf("\n");
+  }
+}
