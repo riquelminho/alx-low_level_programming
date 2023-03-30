a681ecc1ca72254e341af7df7a0829febad4e665
rachid <rachidriquelmi@gmail.com>
2023-03-30 09:26:46 -0700

    new update

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,22 +1,22 @@
 #include "main.h"
 /**
- * _strncat - concatenate strings, defining the size of second string.
- * @dest: string with concatenation
- * @src: string to be concatenated
- * @n: size of second string
- * Return: Always 0.
- */
+* _strncat - concatenate strings, defining the size of second string.
+* @dest: string with concatenation
+* @src: string to be concatenated
+* @n: size of second string
+* Return: Always 0.
+*/
 char *_strncat(char *dest, char *src, int n)
 {
-  while (*dest != '\0')
-    dest++;
-  while (n > 0 && *src != '\0')
-  {
-    *dest = *src;
-    dest++;
-    src++;
-    n--;
-  }
-  *dest = '\0';
-  return (dest);
+while (*dest != '\0')
+dest++;
+while (n > 0 && *src != '\0')
+{
+*dest = *src;
+dest++;
+src++;
+n--;
+}
+*dest = '\0';
+return (dest);
 }
