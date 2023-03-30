cabb49a8fb087823a3dad2406e5043d921ec558c
Rachid.EL <riquelminho@users.noreply.github.com>
2023-03-30 15:54:21 +0000

    Update 101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_number - print an int numbers.
+ * @n: number tested
+ * Return: Always 0.
+ */
+void print_number(int n)
+{
+  if (n < 0)
+  {
+    putchar('-');
+    n = -n;
+  }
+
+  if (n >= 10)
+  {
+    print_number(n / 10);
+  }
+
+  putchar('0' + (n % 10));
+}
