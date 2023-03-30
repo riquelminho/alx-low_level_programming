a80c4e2d9e56a9a2b1b43a391f6b0369864954c4
rachid <rachidriquelmi@gmail.com>
2023-03-30 09:19:06 -0700

    new update

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,65 +1,61 @@
 #include "main.h"
 /**
- * print_buffer - add 2 strings.
- * @b: string1.
- * @size: string2.
- * Return: String with all letters in ROT13 base.
- */
+* print_buffer - add 2 strings.
+* @b: string1.
+* @size: string2.
+* Return: String with all letters in ROT13 base.
+*/
 void print_buffer(char *b, int size)
 {
-  int i, j;
-  for (i = 0; i < size; i += 10)
-  {
+int i, j;
+for (i = 0; i < size; i += 10)
+{
 
-    printf("%08x: ", i);
-
-
-    for (j = 0; j < 10; j++)
-    {
-      if (i + j < size)
-      {
-        printf("%02x ", (unsigned char)b[i + j]);
-      }
-      else
-      {
-        printf("   ");
-      }
-
-      if (j == 4)
-      {
-        printf(" "); 
-      }
-    }
+printf("%08x: ", i);
 
 
-    printf(" ");
-    for (j = 0; j < 10; j++)
-    {
-      if (i + j < size)
-      {
-        char c = b[i + j];
-        if (c >= ' ' && c <= '~')
-        {
-          printf("%c", c);
-        }
-        else
-        {
-          printf(".");
-        }
-      }
-      else
-      {
-        printf(" ");
-      }
-    }
+for (j = 0; j < 10; j++)
+{
+if (i + j < size)
+{
+printf("%02x ", (unsigned char)b[i + j]);
+}
+else
+{
+printf("   ");
+}
 
-    // Print the newline character at the end of the line
-    printf("\n");
-  }
+if (j == 4)
+{
+printf(" "); 
+}
+}
 
-  // Print a newline character if the buffer is empty or incomplete
-  if (size <= 0 || i % 10 != 0)
-  {
-    printf("\n");
-  }
+
+printf(" ");
+for (j = 0; j < 10; j++)
+{
+if (i + j < size)
+{
+char c = b[i + j];
+if (c >= ' ' && c <= '~')
+{
+printf("%c", c);
+}
+else
+{
+printf(".");
+}
+}
+else
+{
+printf(" ");
+}
+}
+printf("\n");
+}
+if (size <= 0 || i % 10 != 0)
+{
+printf("\n");
+}
 }
