62b65d07d36cfd3cb441ebea02780b86adccca27
Rachid.EL <riquelminho@users.noreply.github.com>
2023-04-03 17:20:10 +0000

    Update 7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+  * print_chessboard - Print the chessboard
+  * @a: array of pieces
+  *
+  * Return: Nothing.
+  */
+void print_chessboard(char (*a)[8])
+{
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+            _putchar(a[i][j]);
+            _putchar(' ');
+        }
+        _putchar('\n');
+    }
+}
