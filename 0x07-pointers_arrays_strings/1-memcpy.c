6769d9f8c9037a3a717c049e76a1ec57959c259b
rachid <rachidriquelmi@gmail.com>
2023-04-03 10:58:27 -0700

    new commit

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
 /**
-  * _memcpy - copy memory area
-  * @dest: dest memory area
-  * @src: source memory area
-  * @n: bytes from memory area to copy
-  *
-  * Return: memory area replaced
-  */
+* _memcpy - copy memory area
+* @dest: dest memory area
+* @src: source memory area
+* @n: bytes from memory area to copy
+*
+* Return: memory area replaced
+*/
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i;
+unsigned int i;
 
-	for (i = 0; i < n; i++)
-	{
-		dest[i] = src[i];
-	}
+for (i = 0; i < n; i++)
+{
+dest[i] = src[i];
+}
 
-	return (dest);
+return (dest);
 }
--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,32 +1,32 @@
 #include "main.h"
 
 /**
-  * _strspn - search a string for a set of bytes
-  * @s: source string
-  * @accept: accepted string
-  *
-  * Return: number of bytes in the init segment
-  */
+* _strspn - search a string for a set of bytes
+* @s: source string
+* @accept: accepted string
+*
+* Return: number of bytes in the init segment
+*/
 unsigned int _strspn(char *s, char *accept)
 {
-  unsigned int x = 0, y, z = 0;
+unsigned int x = 0, y, z = 0;
 
-  while (accept[x])
-  {
-    y = 0;
+while (accept[x])
+{
+y = 0;
 
-    while (s[y] != 32)
-    {
-      if (accept[x] == s[y])
-      {
-        z++;
-      }
+while (s[y] != 32)
+{
+if (accept[x] == s[y])
+{
+z++;
+}
 
-      y++;
-    }
+y++;
+}
 
-    x++;
-  }
+x++;
+}
 
-  return (z);
+return (z);
 }
