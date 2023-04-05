e648f778ce674634e6c3217ab0a70c39a4a64387
Rachid.EL <riquelminho@users.noreply.github.com>
2023-04-05 17:59:34 +0000

    Update 3-factorial.c

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+  * factorial - Calculate the factorial of a number
+  * @n: the number to calculate the factorial
+  *
+  * Return: integer value
+  */
+int factorial(int n)
+{
+  if (n < 0)
+  {
+    return (-1);
+  }
+
+  else if (n > 1)
+  {
+    int result;
+    result = n * factorial(n - 1);
+    return (result);
+  }
+  else
+  {
+    return (1);
+  }
+}
