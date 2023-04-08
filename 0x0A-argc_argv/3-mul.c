79fc26839753374662aa171607385b1a80f77104
Rachid.EL <riquelminho@users.noreply.github.com>
2023-04-08 15:44:39 +0000

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -14,13 +14,15 @@ int main(int argc, char *argv[])
 if (argc != 3)
 {
 printf("Error\n");
-return 1;
+return (1);
 }
+else
+{  
 int num1 = atoi(argv[1]);
 int num2 = atoi(argv[2]);
 
 int result = num1 * num2;
 printf("%d\n", result);
-
+}
 return (0);
 }
