a3870d43342d0dc6e8e2c535d66133d77cff201f
rachid <rachidriquelmi@gmail.com>
2023-04-07 18:53:30 -0700

    static_libraries

---
+++ 0x09-static_libraries/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* _strcmp - compare 2 strings.
+* @s1: string to be compared.
+* @s2: string for comparation.
+* Return: Difference.
+*/
+int _strcmp(char *s1, char *s2)
+{
+int i;
+
+for (i = 0; s1[i] == s2[i]; i++)
+{
+
+if (s1[i] == '\0')
+{
+return (0);
+}
+}
+
+return (s1[i] - s2[i]);
+}
