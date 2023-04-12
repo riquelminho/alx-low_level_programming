d7ee5283262dfd33f2af24d0b1876250c4bd966c
Rachid.EL <riquelminho@users.noreply.github.com>
2023-04-12 01:56:25 +0000

    Create 101-strtow.c

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * _isspace - check if a character is whitespace
+ * @c: the character to check
+ *
+ * Return: 1 is c is a whitespace character, otherwise 0
+ */
+int _isspace(int c)
+{
+
+}
+
+
+/**
+ * strtow - split a string into words
+ * @str: a pointer to the string to split
+ *
+ * Return: NULL if memory allocation fails or if str is NULL or empty (""),
+ * otherwise return a pointer to the array of words terminated by a NULL
+ */
+char **strtow(char *str)
+{
+	
+}
