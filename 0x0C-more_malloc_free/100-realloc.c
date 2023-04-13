0ddc01acee3e568297932a4ce080e18d1f4ad0cc
Rachid.EL <riquelminho@users.noreply.github.com>
2023-04-13 17:25:09 +0000

    Update 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,31 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
 
-/**
- * array_range - creates an array of integers containing values from min to max
- * @min: the minimum value to include in the array
- * @max: the maximum value to include in the array
- *
- * Return: pointer to the newly created array or NULL if failed
- */
-int *array_range(int min, int max)
-{
-        int *a, i = 0;
-
-        if (min > max)
-                return (NULL);
-
-        a = malloc((sizeof(int) * (max - min + 1)));
-
-        if (a == NULL)
-                return (NULL);
-
-        while (min <= max)
-        {
-                a[i] = min;
-                i++;
-                min++;
-        }
 
         return (a);
 }
