e149feb1778cc0e55d749837a947628786b3b4b9
Rachid.EL <riquelminho@users.noreply.github.com>
2023-04-15 01:14:05 +0000

    Update 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,45 +1,46 @@
 #include "main.h"
 #include <stdlib.h>
 
+
 /**
-* _realloc - reallocates a memory block using malloc and free
-* @ptr: pointer to the memory previously allocated with malloc
-* @old_size: size of the allocated space for ptr
-* @new_size: new size of the memory block
-*
-* Return: pointer to the newly allocated memory block
-*/
+ * _realloc - reallocates a memory block using malloc and free
+ * @ptr: pointer to the memory block to reallocate
+ * @old_size: size of the allocated space for ptr
+ * @new_size: new size of the memory block
+ *
+ * Return: pointer to the reallocated memory block or NULL if failed
+ */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-void *new_ptr;
+  char *new_ptr, *old_ptr;
+  unsigned int i;
 
-if (new_size == old_size)
-return (ptr);
+  if (new_size == 0)
+  {
+    free(ptr);
+    return (NULL);
+  }
 
-if (ptr == NULL)
-{
-new_ptr = malloc(new_size);
-if (new_ptr == NULL)
-return (NULL);
-return (new_ptr);
-}
+  if (ptr == NULL)
+  {
+    new_ptr = malloc(new_size);
+    if (new_ptr == NULL)
+      return (NULL);
+    return ((void *)new_ptr);
+  }
 
-if (new_size == 0 && ptr != NULL)
-{
-free(ptr);
-return (NULL);
-}
+  if (new_size == old_size)
+    return (ptr);
 
-new_ptr = malloc(new_size);
-if (new_ptr == NULL)
-return (NULL);
+  new_ptr = malloc(new_size);
+  if (new_ptr == NULL)
+    return (NULL);
 
-if (new_size > old_size)
-new_size = old_size;
+  old_ptr = ptr;
+  for (i = 0; i < old_size && i < new_size; i++)
+    new_ptr[i] = old_ptr[i];
 
-for (unsigned int i = 0; i < new_size; i++)
-*((char *)new_ptr + i) = *((char *)ptr + i);
+  free(ptr);
 
-free(ptr);
-return (new_ptr);
+  return ((void *)new_ptr);
 }
