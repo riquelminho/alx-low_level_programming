8a5f32e67273ba37d2fb0471e8feb70afbcff5b8
riquelminho <rachidriquelmi@gmail.com>
2023-05-02 15:06:30 -0700

    new update

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -3,29 +3,29 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- * reverse_listint - Reverses a linked list.
- *
- * @head: A pointer to the first node of the list.
- *
- * Return: A pointer to the first node of the reversed list.
- **/
+* reverse_listint - Reverses a linked list.
+*
+* @head: A pointer to the first node of the list.
+*
+* Return: A pointer to the first node of the reversed list.
+**/
 listint_t *reverse_listint(listint_t **head)
 {
-    listint_t *prev = NULL;
-    listint_t *next = NULL;
+listint_t *prev = NULL;
+listint_t *next = NULL;
 
-    if (*head == NULL)
-        return (NULL);
+if (*head == NULL)
+return (NULL);
 
-    while ((*head)->next != NULL)
-    {
-        next = (*head)->next;
-        (*head)->next = prev;
-        prev = *head;
-        *head = next;
-    }
+while ((*head)->next != NULL)
+{
+next = (*head)->next;
+(*head)->next = prev;
+prev = *head;
+*head = next;
+}
 
-    (*head)->next = prev;
+(*head)->next = prev;
 
-    return (*head);
+return (*head);
 }
--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -3,35 +3,35 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- * free_listint_safe - Frees a listint_t linked list.
- * @h: A pointer to the address of the head of the list.
- *
- * Return: The size of the list that was free'd.
- */
+* free_listint_safe - Frees a listint_t linked list.
+* @h: A pointer to the address of the head of the list.
+*
+* Return: The size of the list that was free'd.
+*/
 size_t free_listint_safe(listint_t **h)
 {
-    size_t count = 0;
-    listint_t *temp, *current;
+size_t count = 0;
+listint_t *temp, *current;
 
-    if (h == NULL || *h == NULL)
-        return (0);
+if (h == NULL || *h == NULL)
+return (0);
 
-    current = *h;
-    while (current != NULL)
-    {
-        count++;
-        if (current <= current->next)
-        {
-            temp = current;
-            current = current->next;
-            free(temp);
-        }
-        else
-        {
-            free(current);
-            break;
-        }
-    }
-    *h = NULL;
-    return (count);
+current = *h;
+while (current != NULL)
+{
+count++;
+if (current <= current->next)
+{
+temp = current;
+current = current->next;
+free(temp);
+}
+else
+{
+free(current);
+break;
+}
+}
+*h = NULL;
+return (count);
 }
