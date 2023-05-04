644f56e3bce1be2dc6df27202da3d17552fb652c
Rachid.EL <riquelminho@users.noreply.github.com>
2023-05-04 23:11:44 +0100

    Update 100-get_endianness.c

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -7,11 +7,7 @@
  */
 int get_endianness(void)
 {
-	unsigned int i = 1;
-	char *c = (char *)&i;
+	int n = 1;
 
-	if (*c)
-		return (1);  /* Little endian */
-	else
-		return (0);  /* Big endian */
+	return (*((char *) &n) + '0');
 }
