552e90a3198f5f8452034fa33cb83332a8d0f016
Rachid.EL <riquelminho@users.noreply.github.com>
2023-05-04 22:41:34 +0100

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,8 +10,30 @@
  */
 void print_binary(unsigned long int n)
 {
-	if (n > 1)
-		print_binary(n >> 1);
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
 
-	_putchar((n & 1) ? '1' : '0');
+	_divide(n);
+}
+
+/**
+  * _divide - ...
+  * @n: ...
+  *
+  * Return: ...
+  */
+void _divide(unsigned long int n)
+{
+	if (n < 1)
+		return;
+
+	_divide(n >> 1);
+
+	if (n & 1)
+		_putchar('1');
+	else
+		_putchar('0');
 }
